68b71b6470df592526429df9765f88547629d9b5
Vasile Mosan <vasilemosan@gmail.com>
2021-03-18 20:44:21 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// The below variable represents my age
+const myAge = 31;
+// The below variable will be used latter
+let earlyYears = 2;
+// The below variable is a conversion of the first two years of a dog’s life
+earlyYears = earlyYears * 10.5; 
+
+// later Years variable
+let laterYears = myAge - 2;
+// number of dog years converted to my years
+laterYears = laterYears  * 4;
+console.log("Early dog Years are " + earlyYears + "\r Later dog Years are " +  laterYears );
+// The below variable is a conversion man years into dogs years
+myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Vasile'; 
+// The below method writes my name in lowercase
+console.log("my Name displayed in lowercase: " + myName.toLowerCase());
+// The below line of code displays the conversion of my ears converted to dog years
+console.log("My name is " + myName + ". I am " + myAge + " years old in human years which is " + myAgeInDogYears + " years old in dog years");
